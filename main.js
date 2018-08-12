1f7d80b96a2941ce6daebbee5df011b3ebe7c8b3
nacu411 <kfedorov78@gmail.com>
2018-08-13 00:43:38 +0300

    Happy birthday

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-document.onclick = function(){
-	document.body.style.background = "#f1f1f1";
-	document.body.style.color = "#3d3d3d";
-};
+alert("hello world");
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function validation() {
+	var validate = prompt("Пароль от блокировки твоего айфона", "Ответ");
+
+	if(validate == '1819'){
+		console.log("Чмок")
+	} else{
+		window.location = 'https://google.com';
+	}
+}
+
+validation();
